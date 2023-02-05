e33a65dbc1a3b2bba7702c8ed0c94cc8392818e4
daisuke_karasawa <daisuke.0208.k@icloud.com>
2023-02-05 22:55:28 +0900

    AnimalからAnimalsへの変更

--- spec/animals_spec.rb
+++ spec/animals_spec.rb
@@ -2,28 +2,28 @@
 require_relative '../animals'
 
 # 動物
-describe Animal do
-  let(:animal) { Animal.new }
+describe Animals do
+  let(:animals) { Animals.new }
 
   example '動物は鳴く' do
-    expect(animal.sound).to eq '動物は鳴きます。'
+    expect(animals.sound).to eq '動物は鳴きます。'
   end
 end
 
 # 犬
 describe Dog do
-  let(:animal) { Dog.new }
+  let(:animals) { Dog.new }
 
   example '犬はワンワンと鳴く' do
-    expect(animal.sound).to eq '犬はワンワンと鳴きます。'
+    expect(animals.sound).to eq '犬はワンワンと鳴きます。'
   end
 end
 
 # 猫
 describe Cat do
-  let(:animal) { Cat.new }
+  let(:animals) { Cat.new }
 
   example '猫はニャーと鳴く' do
-    expect(animal.sound).to eq '猫はニャーと鳴きます。'
+    expect(animals.sound).to eq '猫はニャーと鳴きます。'
   end
 end
