d6ceac4749b0e44c1cb6ebb911813835bf83deda
daisuke_karasawa <daisuke.0208.k@icloud.com>
2023-02-05 23:20:06 +0900

    state_patternへファイル/ディレクトリを移動

--- state_pattern/people.rb
+++ state_pattern/people.rb
@@ -1,10 +1,13 @@
 require_relative './animal'
+
 # 動物インスタンス
 animal = Animal.new
 puts animal.action
+
 # 犬インスタンス
 animal.kind = Dog.new
 puts animal.action
+
 # 猫インスタンス
 animal.kind = Cat.new
 puts animal.action
