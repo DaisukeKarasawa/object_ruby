0e45b1ce3bfdc2f755599c43474f8a05fe11aa90
daisuke_karasawa <daisuke.0208.k@icloud.com>
2023-02-13 13:28:07 +0900

    コメントの編集

--- factory_pattern/spec/zoo_factory_spec.rb
+++ factory_pattern/spec/zoo_factory_spec.rb
@@ -3,11 +3,11 @@ require_relative '../zoo_factory'
 describe Zoo do
   let(:animals) { Zoo.new }
 
-  example '飼っている動物が犬だった場合、ワンワンと鳴く' do
+  example '動物が犬だった場合、ワンワンと鳴く' do
     expect(animals.born('Dog')).to eq '犬はワンワンと鳴きます。'
   end
 
-  example '飼っている動物が猫だった場合、ニャーと鳴く' do
+  example '動物が猫だった場合、ニャーと鳴く' do
     expect(animals.born('Cat')).to eq '猫はニャーと鳴きます。'
   end
 end
